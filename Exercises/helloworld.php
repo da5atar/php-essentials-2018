98dd30355f2cdf55563850c6b20b78a35c06aeb4
da5atar <mass.sow@gmail.com>
2018-02-24 20:37:22 +0000

    First commit: yet another PHP Essentials course

---
+++ Exercises/helloworld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+	"http://www.w3.org/TR/html4/loose.dtd">
+	
+<html lang="en">
+	<head>
+		<title>Hello World</title>  
+	</head>
+	<body>
+		<?php // This is a single line comment ?>
+		<?php # This is also a single line comment ?>
+		<?php echo("Hello World!"); ?>
+		<br />
+		<?php echo "This is" . " PHP!"; ?>
+		<br />
+		<?php echo 2 + 3; ?>
+		<?php /* Double-line comments are
+			writen like this.
+		*/?>
+	</body>
+</html>
